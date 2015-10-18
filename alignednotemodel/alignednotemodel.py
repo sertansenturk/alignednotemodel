581a982ae8219a7bdefcb27fb7a9838f021792ef
Sertan Senturk <sertansenturk_mtg@Sertans-MTG-MacBook-Pro.local>
2015-10-18 02:51:21 +0200

    Added the complete histogram

--- alignednotemodel/alignednotemodel.py
+++ alignednotemodel/alignednotemodel.py
@@ -2,10 +2,9 @@ import numpy as np
 import PitchDistribution
 
 def getModels(pitch, alignednotes, kernel_width=2.5):
-	dummyFreq = 440.0
 	noteNames = set(an['Symbol'] for an in alignednotes)
-	noteModels = dict((nn, {'notes':[], 'distribution':[], 'stablepitch':[], 
-		'numsamples':[]}) for nn in noteNames)
+	noteModels = dict((nn, {'notes':[], 'distribution':[], 
+		'stablepitch':[]}) for nn in noteNames)
 
 	# compute note trajectories and add to each model
 	for an in alignednotes:
@@ -21,22 +20,28 @@ def getModels(pitch, alignednotes, kernel_width=2.5):
 		tempPitchVals = np.hstack(nn['trajectory'][:,1] 
 			for nn in noteModels[key]['notes'])
 
-		noteModels[key]['numsamples'] = len(tempPitchVals)
-
-		tempCentVals = PitchDistribution.hz_to_cent(tempPitchVals, dummyFreq)
-		noteModels[key]['distribution'] = PitchDistribution.generate_pd(
-			tempCentVals, ref_freq=dummyFreq, kernel_width=kernel_width, 
-			step_size=7.5)
-		noteModels[key]['distribution'].bins = PitchDistribution.cent_to_hz(
-			noteModels[key]['distribution'].bins, dummyFreq)
-
-	# scale according to relative usage of each note
-	totalNumSamples = sum(nm['numsamples'] for nm in noteModels.values())
-	for key in noteModels.keys():
-		noteModels[key]['distribution'].vals = (noteModels[key]['distribution'].vals 
-			* noteModels[key]['numsamples'] / totalNumSamples)
+		noteModels[key]['distribution']=getModelDistribution(tempPitchVals)
 
 	# get the stable pitch
 
+	# get the complete histogram
+	noteModels['all'] = {'distribution':getModelDistribution(pitch[:,1]), 
+						'notes':None, 'stablepitch': None}
+
+	# scale according to relative usage of each note
+	#for key in noteModels.keys():
+	#	noteModels[key]['distribution'].vals = (noteModels[key]['distribution'].vals 
+	#		* noteModels[key]['numsamples'] / totalNumSamples)
+
 	return noteModels
 
+def getModelDistribution(pitchVals, kernel_width=2.5):
+	dummyFreq = 440.0
+	step_size = 7.5
+	tempCentVals = PitchDistribution.hz_to_cent(pitchVals, dummyFreq)
+	distribution = PitchDistribution.generate_pd(tempCentVals, 
+		ref_freq=dummyFreq, kernel_width=kernel_width, step_size=step_size)
+	distribution.bins = PitchDistribution.cent_to_hz(distribution.bins, 
+		dummyFreq)
+
+	return distribution
