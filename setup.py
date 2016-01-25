8ab62825a09fa10a067ad88be17bcb268b8d253e
Sertan Senturk <contact@sertansenturk.com>
2016-01-25 02:05:22 +0100

    Updated setup.py

--- setup.py
+++ setup.py
@@ -3,9 +3,12 @@
 from setuptools import setup
 
 setup(name='aligned-note-models',
-      version='0.1',
-      description='Tools to compute models for notes from audio-score alignment results',
+      version='1.0',
       author='Sertan Senturk',
+      author_email='contact AT sertansenturk DOT com',
+      license='agpl 3.0',
+      description='Tools to compute models for notes from audio-score alignment results',
       url='http://sertansenturk.com',
       packages=['alignednotemodel'],
 )
+
